52c95cae31d58c1e8871a2d20c25b94d52039046
dva <60975283+dvaRepo@users.noreply.github.com>
2021-11-12 15:07:50 -0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,2 +1,21 @@
 # install packages
 xarg -a packages.txt sudo apt install -y
+
+
+# fonts
+# make an ubuntu font folder
+sudo mkdir /usr/share/fonts/truetype/ubuntu-fonts
+# download ubuntu font family
+wget https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip
+#unzip downloaded file
+unzip *.zip
+# change directories into unzipped ubuntu font folder
+cd ubuntu-font-family*
+# move everything ending in ".ttf"  into the ubuntu font folder we created earlier
+sudo mv *.ttf /usr/share/fonts/truetype/ubuntu-fonts/
+# change directories back to home
+cd ..
+# remove all files ending in ".zip"
+rm *.zip
+# remove all folders beginning with "ubuntu-font-family"
+rm -r ubuntu-font-family*
