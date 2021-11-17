7dcd76e139bb084683e829b4da9d29cad479446f
dva <60975283+dvaRepo@users.noreply.github.com>
2021-11-16 17:08:48 -0800

    Update themes.sh

--- themes.sh
+++ themes.sh
@@ -1,5 +1,12 @@
-
+# gtk theme
 git clone https://github.com/vinceliuice/Orchis-theme.git
 cd Orchis-theme/
 ./install.sh --tweaks compact
+cd ..
+
+
+# icon theme
+git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
+cd Reversal-icon-theme/
+./install.sh -blue
 cd ..
